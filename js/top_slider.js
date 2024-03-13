0d077733d8d9db0ab6ed8f056371e857c4e6539f
mugi-taro <komugi.taro0813@gmail.com>
2024-03-13 11:07:52 +0900

    写真の追加

--- js/top_slider.js
+++ js/top_slider.js
@@ -1,8 +1,35 @@
-// スライダー設定
-const swiper = new Swiper(".swiper", {
+/*--------------------------------
+スライダー設定
+---------------------------------*/
+// KV（PC）
+const sliderKvPc = new Swiper(".slider-kv_pc", {
+
+    autoplay: {
+        delay: 5000, //3秒ごとにスライダを切り替える
+        disableOnInteraction: false //スライドに触っても自動再生を停止しない
+    },
+
+    effect: "fade",
+
+});
+
+// KV（PC）
+const sliderKvSp = new Swiper(".slider-kv_sp", {
+
+    autoplay: {
+        delay: 3000, //3秒ごとにスライダを切り替える
+        disableOnInteraction: false //スライドに触っても自動再生を停止しない
+    },
+
+    effect: "fade",
+
+});
+
+// デイリーセクション
+const sliderDaily = new Swiper(".slider-daily", {
     loop: false,
 
-    centeredSlides: true, 
+    centeredSlides: true,
     slidesPerView: 1, //ライドを1枚同時表示
     spaceBetween: 60, //スライド間の距離
     speed: 600, //スライドの推移時間

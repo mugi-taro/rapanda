76e4978ad4b4e402543cfc5ec580808899a7e53c
mugi-taro <komugi.taro0813@gmail.com>
2024-06-10 10:44:17 +0900

    アナリティクス設置

--- php/header.php
+++ php/header.php
@@ -24,6 +24,16 @@
 
 </head>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-83RRMLZ10F"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-83RRMLZ10F');
+</script>
+
 <body>
   <!-- ヘッダー -->
   <header class="rp-header">
